44df94dc1f52ccfba8421ea106664088d9dff52c
zhisk <zhisk@xuele.net>
2020-10-10 16:24:34 +0800

    add Console.java

---
+++ src/main/java/example/thing/Console.java
@@ -0,0 +1,31 @@
+package example.thing;
+
+import java.util.Scanner;
+
+/**
+ * 控制台
+ */
+public class Console {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("console>");
+
+            String cmd;
+            while (scanner.hasNextLine()) {
+                cmd = scanner.nextLine();
+                switch (cmd) {
+                    case "exit": {
+                        System.exit(0);
+                    }
+                    break;
+                    default: {
+                        System.out.println("console>can't parse [" + cmd + "]");
+                    }
+                }
+                System.out.print("console>");
+            }
+        }).start();
+    }
+}
